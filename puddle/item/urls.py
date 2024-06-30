bc202a264a218e3722dbca2bcbc5f984dcb5b07e
waitorgr <github0505@gmail.com>
2024-06-30 04:54:20 +0300

    create search (need refact)

--- puddle/item/urls.py
+++ puddle/item/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name= 'item'
 
 urlpatterns=[
+    path('',views.items,name='items'),
     path('<int:pk>',views.detail,name='detail'),
-    path('new/', views.new, name='new')
+    path('new/', views.new, name='new'),
 ]
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,8 +1,58 @@
 from django.shortcuts import render,get_object_or_404,redirect
-from .models import Item,ItemImage
+from .models import Item,ItemImage,Category,Producer,Teg,Seria
+from django.db.models import Q
 from django.contrib.auth.decorators import user_passes_test
 
 from .forms import ItemWithImagesForm, ItemImageForm,ProducerForm,TegForm,SeriaForm
+
+
+def items(request):
+    items= Item.objects.filter(is_sold=False)
+
+    category_id = request.GET.get('category', 0)
+    categories= Category.objects.all()
+
+    producer_id = request.GET.get('producer', 0)
+    producer= Producer.objects.all()
+
+    teg_ids = request.GET.getlist('teg')
+    teg= Teg.objects.all()
+
+    seria_id = request.GET.get('seria', 0)
+    seria= Seria.objects.all()
+
+    query= request.GET.get('query','')
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if producer_id:
+        items = items.filter(producer_id=producer_id)
+
+    if teg_ids:
+        if teg_ids and teg_ids[0] != '':
+            teg_ids = list(map(int, teg_ids))
+            items = items.filter(tegs__id__in=teg_ids).distinct()
+
+    if seria_id:
+        items = items.filter(seria_id=seria_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) )
+
+    return render(request,'item/items.html',{
+        'items':items,
+        'query':query,
+        'categories':categories,
+        'producer':producer,
+        'teg':teg,
+        'seria':seria,
+        'category_id':int(category_id),
+        'producer_id':int(producer_id),
+        'teg_ids': teg_ids,
+        'seria_id':int(seria_id),
+    })
+
 
 def detail(request, pk):  #зробити для багатьох картинок
     item=get_object_or_404(Item,pk=pk)
